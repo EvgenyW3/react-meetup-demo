1f1725d1f44b617c1c02179983b06ea92f7f2e41
EvgenyW3 <hnv4032@C02WG9D3HV2L.home>
2018-11-04 18:32:46 +0200

    Common way of using Apollo client with React app

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import PhoneList from "./components/PhoneList";
-import PhoneListModel from "./components/PhoneListModel";
-
-const store = new PhoneListModel();
 
 export const client = new ApolloClient({
   uri: "https://graphql-react-meetup.herokuapp.com/"
@@ -12,12 +9,12 @@ export const client = new ApolloClient({
 
 function App() {
   return (
-    // <ApolloProvider client={client}>
-    //   <div>
-    //     <h2>Hello React people!</h2>
-    //   </div>
-    <PhoneList phoneListModel={store} />
-    // </ApolloProvider>
+    <ApolloProvider client={client}>
+      <div>
+        <h2>Hello React people!</h2>
+      </div>
+      <PhoneList />
+    </ApolloProvider>
   );
 }
 
--- client/src/components/PhoneList.js
+++ client/src/components/PhoneList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import { observer } from "mobx-react";
 
 export const getPhones = gql`
   {
@@ -16,38 +15,27 @@ export const getPhones = gql`
   }
 `;
 
-function PhoneList({ phoneListModel }) {
-  return phoneListModel.phones.map(
-    ({ id, brand, model, price, image, tag }) => (
-      <ul key={id}>
-        <li>{brand}</li>
-        <li>{model}</li>
-        <li>{price}€</li>
-        <li>
-          <img src={image} alt="phone" />
-        </li>
-        <li>{tag}</li>
-      </ul>
-    )
-  );
-  // <Query query={getPhones}>
-  //   {({ loading, error, data }) => {
-  //     if (loading) return <p>Lodaing...</p>;
-  //     if (error) return <p>You are in trouble!</p>;
+function PhoneList() {
+  return (
+    <Query query={getPhones}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Lodaing...</p>;
+        if (error) return <p>You are in trouble!</p>;
 
-  //     return data.getPhones.map(({ id, brand, model, price, image, tag }) => (
-  //       <ul key={id}>
-  //         <li>{brand}</li>
-  //         <li>{model}</li>
-  //         <li>{price}€</li>
-  //         <li>
-  //           <img src={image} alt="phone" />
-  //         </li>
-  //         <li>{tag}</li>
-  //       </ul>
-  //     ));
-  //   }}
-  // </Query>
+        return data.getPhones.map(({ id, brand, model, price, image, tag }) => (
+          <ul key={id}>
+            <li>{brand}</li>
+            <li>{model}</li>
+            <li>{price}€</li>
+            <li>
+              <img src={image} alt="phone" />
+            </li>
+            <li>{tag}</li>
+          </ul>
+        ));
+      }}
+    </Query>
+  );
 }
 
-export default observer(PhoneList);
+export default PhoneList;
