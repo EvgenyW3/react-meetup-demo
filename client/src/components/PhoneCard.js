10638ec3b7de15526fb595457fc2343e33e7b826
EvgenyW3 <hnv4032@host-169-164.south.sonera.fi>
2018-11-08 11:03:29 +0200

    Last improvements before presentation

--- client/src/components/PhoneCard.js
+++ client/src/components/PhoneCard.js
@@ -3,16 +3,14 @@ import React from "react";
 function PhoneCard({ phone: { brand, model, image, price, tag } }) {
   return (
     <div className="phone-card-container">
-      <div className="tag">
-        <span>{tag}</span>
-      </div>
+      <div className="tag">{tag && <span>{tag}</span>}</div>
       <div className="image-container">
         <div className="image" style={{ backgroundImage: `url(${image})` }} />
       </div>
 
       <div className="brand">{brand}</div>
       <div className="model">{model}</div>
-      <div className="price">{price} €</div>
+      {price && <div className="price">{price} €</div>}
     </div>
   );
 }
